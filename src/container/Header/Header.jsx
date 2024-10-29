18507564be4f3fe355356f99ae41d4fc7526f64b
anand <mgsachidananda@gmail.com>
2024-10-29 15:10:25 +0530

    changes

--- src/container/Header/Header.jsx
+++ src/container/Header/Header.jsx
@@ -7,8 +7,8 @@ import './Header.css';
 const Header = () => (
   <div className="app__header app__wrapper section__padding" id="home">
     <div className="app__wrapper_info">
-      <SubHeading title="ಚಹಾ - ಭರವಸೆಗಳ ದಾರಿಯಲ್ಲಿ ಒಂದು ಸಣ್ಣ ವಿರಾಮ" />
-      <h1 className="app__header-h1">CAFE UNNATI</h1>
+      <SubHeading style={{fontFamily:'Open Sans, sans-serif'}}title="ಚಹಾ - ಭರವಸೆಗಳ ದಾರಿಯಲ್ಲಿ ಒಂದು ಸಣ್ಣ ವಿರಾಮ" />
+      <h1 className="app__header-h1" style={{fontFamily:'Open Sans, sans-serif'}}>CAFE UNNATI</h1>
       <p className="p__opensans" style={{ margin: '2rem 0' }}>Welcome to Cafe Unnati, where every sip is brewed with love, and every moment feels like home. Your happiness is our favorite flavor!</p>
       <a href='#menu'><button type="button" className="custom__button" href="#menu">Explore Menu</button></a>
     </div>
